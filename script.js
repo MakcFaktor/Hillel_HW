fd3b19f7a3a2909f0d9ef17beb3f0dd2bacf8c82
Makcim Motruk <56037758+MakcFaktor@users.noreply.github.com>
2023-10-15 17:07:13 +0300

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-const input1 = document.getElementById('input1');
-const input2 = document.getElementById('input2');
-const input3 = document.getElementById('input3');
-const output = document.getElementById('output');
+const rangeInput = document.getElementById('rangeInput');
+const numberInput = document.getElementById('numberInput');
+const greenBlock = document.getElementById('greenBlock');
+const redBlock = document.getElementById('redBlock');
 
-let previousValues = '';
+function updateDiagram(value) {
+    const commissionRates = {
+        20: 0.002,
+        50: 0.004,
+        75: 0.006,
+        100: 0.008
+    };
 
-function checkInputs() {
-    const currentValues = `${input1.value}, ${input2.value}, ${input3.value}`;
+    let commission = 0;
+    let commissionHeight = 0;
 
-    if (currentValues !== previousValues) {
-        output.value = currentValues;
-        previousValues = currentValues;
+    for (const rate in commissionRates) {
+        if (value <= parseFloat(rate)) {
+            commission = commissionRates[rate];
+            commissionHeight = value * commission * 10;
+            break;
+        }
     }
+
+    const totalAmount = parseFloat(value) + parseFloat(value) * commission;
+    redBlock.style.height = ` ${commissionHeight}px`;
+    greenBlock.style.height = `${value}px`;
+
+    console.log(`Комісія: ${commission * 100}%, Загальна кількість: ${totalAmount}`);
 }
 
-const interval = setInterval(checkInputs, 2000);
+rangeInput.addEventListener('input', (event) => {
+    const value = event.target.value;
+    numberInput.value = value;
+    updateDiagram(value);
+});
+
+numberInput.addEventListener('input', (event) => {
+    const value = event.target.value;
+    rangeInput.value = value;
+    updateDiagram(value);
+});
