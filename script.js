eef9c0ff2c76135155ac8c039eee3d681119d28a
Makcim Motruk <56037758+MakcFaktor@users.noreply.github.com>
2023-10-26 23:07:21 +0300

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-function displayRandomImage() {
-    const imagesFolder = 'img/';
-    const imageContainer = document.getElementById('imageContainer');
+let table = document.getElementById("myTable");
 
-    const randomImageNumber = Math.floor(Math.random() * 9) + 1;
-    const imageElement = document.createElement('img');
+table.addEventListener("click", function (event) {
+    let target = event.target;
+    if (target.tagName === "TD") {
+        let originalText = target.textContent;
+        let textarea = document.createElement("textarea");
+        textarea.value = originalText;
 
-    imageElement.src = `${imagesFolder}${randomImageNumber}.jpg`;
-    imageElement.alt = `Зображення ${randomImageNumber}`;
+        let saveButton = document.createElement("button");
+        saveButton.textContent = "Save";
 
-    imageContainer.innerHTML = '';
+        let cancelButton = document.createElement("button");
+        cancelButton.textContent = "Cancel";
 
-    imageContainer.appendChild(imageElement);
-}
+        target.innerHTML = "";
+        target.appendChild(textarea);
+        target.appendChild(saveButton);
+        target.appendChild(cancelButton);
+
+        saveButton.addEventListener("click", function () {
+            target.textContent = textarea.value;
+        });
+
+        cancelButton.addEventListener("click", function () {
+            target.textContent = originalText;
+        });
+    }
+});
